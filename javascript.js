0dcc7b999077d60d0075e45d3aaef34312fab1b2
vcwong99 <vivek0799@gmail.com>
2023-07-27 23:43:10 -0700

    made much more consistant and readable

--- javascript.js
+++ javascript.js
@@ -5,14 +5,23 @@ function getComputerChoice(){
     return Math.floor(Math.random() * 3);
 }
 
+function getPlayerChoice(){
+    let input=prompt("Rock, Paper or Scissors?");
+    input = input.toLowerCase();
+    return (input=="rock")?0:
+        (input=="paper")?1:
+        (input=="scissors")?2:
+        3;
+}
+
 function judge(computer, player){
     if(choice[computer]==player){
         console.log("tie game!");
     }
-    else if(choice[(computer+1)%3]==player){
+    else if(((computer+1)%3)==player){
         console.log("Player Wins!");
     }
-    else if(choice[(computer-1)%3]==[player]){
+    else if(((computer-1)%3)==player){
         console.log("Computer wins!");
     }
 
@@ -20,11 +29,9 @@ function judge(computer, player){
 }
  
  while(keepPlaying){
-    let playerChoice = prompt("Rock, Paper or Scissors?");
-    let computerChoice = getComputerChoice();
-    playerChoice = playerChoice.toLowerCase();
-    console.log("you chose: "+playerChoice);
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice(); 
+    console.log("you chose: "+choice[playerChoice]);
     console.log("the computer chose: "+choice[computerChoice]);
     judge(computerChoice, playerChoice);
-    //keepPlaying = prompt("Rock, Paper or Scissors?") ? 
  }
